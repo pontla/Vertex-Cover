afea9393f709f554de6da04ab0b564181c6586e1
Quentin <quentin@quentin-HP-ProBook-4530s.(none)>
2012-12-03 01:20:19 +0100

    graphAl.cpp

---
+++ src/graphAl.cpp
@@ -0,0 +1,19 @@
+class graphAl
+{
+public:
+    // Constructeur
+    
+    // Destructeur 
+    graphAl:: graphAl(){
+	//TODO
+    }
+    // Méthodes    
+     void creerGraph(int nb_nodes, int nb_edges){
+
+    }
+
+    // Attributs
+private:
+   int nb_nodes;
+   int nb_edges;
+};
