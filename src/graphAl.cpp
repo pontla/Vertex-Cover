9d16c42a95dfedbe1a4d63c7b599ccacadf13acb
Quentin <quentin@quentin-HP-ProBook-4530s.(none)>
2012-12-04 14:45:25 +0100

    arguments OK

--- src/graphAl.cpp
+++ src/graphAl.cpp
@@ -1,19 +1,36 @@
-class graphAl
-{
+/*class graphAl{
 public:
     // Constructeur
-    
-    // Destructeur 
-    graphAl:: graphAl(){
-	//TODO
+    graphAl::graphAl(int n, int p){
+	int cpt=0;
+	nb_nodes=n;
+	nb_edges=p;	
+	for(cpt=0;cpt<n;cpt++) cardG[cpt]=0;
     }
+    // Destructeur 
+     graphAl::~graphAl(){
+	
+    }
+    
     // Méthodes    
-     void creerGraph(int nb_nodes, int nb_edges){
-
+    void  graphAl::creerGraph(int nb_nodes, int nb_edges){
+	
+    }
+    
+    void  graphAl::updateCardinalite(int u, int v){
+	
+    }
+    
+    //Accesseurs 
+     graphAl::setGraph(std::vector<std::vector<int> > & myGraph){
+	myGraph = graph;
     }
 
     // Attributs
+public:
+   std::vector<std::vector<int> > graph;
+   std::vector<int> cardG;
 private:
    int nb_nodes;
    int nb_edges;
-};
+}*/
